88e507ba782cee82f4edb10fc90931fa3d93665c
Tom Kent <TomeKent@hotmail.com>
2014-01-27 16:25:36 +0000

    Proper tracking of boats
    Should now print stuff out properly
    
    Signed-off-by: Tom Kent <TomeKent@hotmail.com>

--- Battleship.py
+++ Battleship.py
@@ -1,5 +1,10 @@
 import numpy, math									# To oo some maths
 from random import randint
+
+
+######################################################
+# 					Set up the board
+######################################################
 grid_size = int(10)
 
 board = []
@@ -10,15 +15,22 @@ for x in range(grid_size):
 
 
 def print_board(board,board_op):
+	header = range(0,grid_size)
 	print "Opponents Board:"
+	print "     ",' '.join(map(str, range(1,grid_size+1)))
+	print "   ","_ " * (grid_size+1)
+	count = 0
 	for row in board_op:
-		print " ".join(row)
-
+		count += 1
+		print count," "*(2-len(str(count))),"|", " ".join(row)
 	print "\n", "##"*(grid_size), "\n"
 	print "Your Guesses:"
-
+	print "     ",' '.join(map(str, range(1,grid_size+1)))
+	print "   ","_ " * (grid_size+1)
+	count = 0
 	for row in board:
-		print " ".join(row)
+		count += 1
+		print count," "*(2-len(str(count))),"|", " ".join(row)
 
 def random_row(board):
 	return randint(0, len(board) - 1)
@@ -26,13 +38,10 @@ def random_row(board):
 def random_col(board):
 	return randint(0, len(board[0]) - 1)
 
-ship_row = random_row(board)
-ship_col = random_col(board)
-# print ship_row
-# print ship_col
 
-# Lets place the Boats!!!! Woooo
-
+###################################################
+# 			Lets place the Boats!!!! Woooo
+###################################################
 Boats_hor = [[[0,0],[0,1]], 									# Horizontal line ##
 			[[0,0],[0,1],[0,2]],								# Horizontal line ###
 			[[0,0],[0,1],[0,2]],								# Horizontal line ###
@@ -55,7 +64,6 @@ Boat_types = [[1,2,"Destroyer"],
 Boat_type_count = []
 for i in range(5):
 	Boat_type_count.append([0,0])
-# print Boat_type_count	
 	
 for j in range(0,len(Boats_hor)):
 	orient = randint(0,1)
@@ -84,45 +92,54 @@ for j in range(0,len(Boats_hor)):
 print "\nLet's play Battleships!\n"
 print_board(board,board_op)
 		
-											
-# PLAY THE GAME!
-
-for turn in range(10):
-	guess_row = int(raw_input("Guess Row:"))-1
-	guess_col = int(raw_input("Guess Col:"))-1
-	
-	Boat_type_count = []
-	for i in range(5):
-		Boat_type_count.append([0,0])
+############################################################												
+#						 PLAY THE GAME!
+############################################################
+def check_shot(guess_row,guess_col):
 		
+	for i in range(len(Boat_type_count)):
+		Boat_type_count[i][0] = 0
+			
 	if (guess_row < 0 or guess_row > grid_size-1) or (guess_col < 0 or guess_col > grid_size-1):
 	   print "Oops, that's not even in the ocean."
-	
-	elif board_op[guess_row][guess_col] != "-": 				# We've Hit Something
-		board[guess_row][guess_col] = "o"
+	elif board_op[guess_row][guess_col] != "-": 				# We've Hit something
+		board[guess_row][guess_col] = "X"
 		print_board(board,board_op)
 		print "Hit!"
 		for n in range(grid_size):
 			for m in range(grid_size):
-				if board[n][m] != "-":
-					# print Boat_type_count[int(board_op[n][m])-1][1]
-					if Boat_type_count[int(board_op[n][m])-1][1] == 0:
-						Boat_type_count[int(board_op[n][m])-1][0] += 1
-		# print Boat_type_count
-		for j in range(5):
-			if Boat_type_count[j][1] == 0:
-				if Boat_type_count[j][0] == int(Boat_types[j][1]):
-					print "You sunk my %s!" % Boat_types[j][2]
-					Boat_type_count[j][1] == 1
-				
+				if board[n][m] =='X':
+					if Boat_type_count[int(board_op[n][m]) - 1][1] == 0:
+						Boat_type_count[int(board_op[n][m]) - 1][0] += 1								
+			for j in range(len(Boat_type_count)):
+				if Boat_type_count[j][1] == 0:
+					if Boat_type_count[j][0] == int(Boat_types[j][1]):
+						print "You sunk my %s!" % Boat_types[j][2]
+						Boat_type_count[j][1] = 1			
 	else:
-		if board[guess_row][guess_col] == "o":
+		if board[guess_row][guess_col] != "-":
 			print "You guessed that one already."
 		else:
-			print "Miss!"
 			board[guess_row][guess_col] = "o"
+			print "Miss!"
 		# print (turn + 1)
 		print_board(board,board_op)
+
+
+Boat_type_count = []
+for i in range(5):
+	Boat_type_count.append([0,0])
 	
-		if turn ==3:
-			print "Game Over"
+	
+for turn in range(10):
+	guess_row = int(raw_input("Guess Row:"))-1
+	guess_col = int(raw_input("Guess Col:"))-1	
+	check_shot(guess_row,guess_col)
+	print "Ships sunk:"
+	for k in range(len(Boat_type_count)):
+		if Boat_type_count[k][1] == 1:
+			print "[X]: %s (len:%s)" %(Boat_types[k][2], str(Boat_types[k][1]))
+		else:
+			print "[ ]: %s (len:%s)" %(Boat_types[k][2], str(Boat_types[k][1]))
+
+print "Game Over"
